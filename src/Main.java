a434c4d9150ae1a2eb2d1fb42d68c31a7d6aa67f
Danil Kobzev <denthenttg@gmail.com>
2024-04-12 23:34:10 +0300

    Add checker, editor and Main file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
+import java.io.FileNotFoundException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
+        String mdFilePath;
+        String outputFilePath;
+        boolean outArgSuccesCondition = (args.length == 3 && args[1].equals("--out"));
+
+        if (args.length == 0) {
+            System.out.println("Enter arguments for program");
+        } else if (args.length == 1) {
+            mdFilePath = args[0];
+            stdOut(mdFilePath);
+        } else if (outArgSuccesCondition) {
+            mdFilePath = args[0];
+            outputFilePath = args[2];
+            saveAsFile(mdFilePath, outputFilePath);
+        } else {
+            System.out.println("Wrong arguments");
+        }
+
+
+    }
+
+    public static void stdOut(String filePath) throws Exception {
+        String fileText = FileScanner.toString(filePath);
+        String[] output = Editor.mdToHtmlStringArr(fileText);
+        outputStrings(output);
+    }
+
+    public static void saveAsFile(String filePath, String outputPath) throws Exception {
+        String fileText = FileScanner.toString(filePath);
+        String output = Editor.mdToHtmlString(fileText);
+        FileSaver.stringToFile(output, outputPath);
+    }
+    public static void outputStrings(String[] array) {
+        for (String str : array) {
+            System.out.println(str);
+        }
     }
 }
